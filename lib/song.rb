f8e940ad7079e13e2aaa9baca07f0ba641465dec
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-13 15:30:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,15 +18,7 @@ class Song
     @@songs
   end
 
-=begin
-    def self.reset_all
-      self.all.clear
-    end
 
-    def self.count
-      self.all.count
-    end
-=end
 
   def artist=(artist)
     @artist = artist
