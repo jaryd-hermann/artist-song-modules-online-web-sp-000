f76716ea476d6d93276111bf36184f0333a7e903
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-13 15:39:09 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,12 +1,12 @@
 module Memorable
   module ClassMethods
 
-    def self.reset_all
+    def reset_all
       @all.clear
     end
 
-    def self.count
+    def count
       @all.count
     end
   end #end ClassMethods
-end #end Memorable 
+end #end Memorable
